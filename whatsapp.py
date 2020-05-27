6d2df40e74a7d2f232d9ffbdde77882e9e30db2c
KaranKamat0506 <karankamat2000@gmail.com>
2020-05-27 14:36:52 +0530

    First Commit

---
+++ whatsapp.py
@@ -0,0 +1,9 @@
+import pywhatkit as kit
+
+#pip install pywhatkit
+kit.sendwhatmsg("+9177********","This is a message from a python script using pywhatkit library",10,45)
+
+#1st argument:Receivers phone number with country code
+#2nd Argument:Message 
+#3rd argument:Time-hour
+#4th argument:Time-ming
